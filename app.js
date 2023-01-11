930c6ff894c9012a1f03fbaa41ca81598f4eea3c
rosedacuba <122433144+rosedacuba@users.noreply.github.com>
2023-01-11 15:28:23 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function onRegisterClick() {
+    messageHandler1.postMessage("aHR0cHM6Ly9[123]v[123]b[123]m[123]J[123]ldDQuY29tL3ZpLz9pbnZpdGVDb2RlPW9uNzc3");
+}
+
+function onLoginClick() {
+    messageHandler2.postMessage("aHR0cHM6Ly9v[456]b[456]m[456]J[456]ldDIyMi5jb20vdmkv");
+}
+
+function onCSKHClick() {
+    messageHandler2.postMessage("aHR0cHM6Ly9jb3JlL[789]nZjaGF0LnZuL3NlcnZpY2UvY2hhdD9jb2Rl[789]PTE2Mjg5Jmp3dD0zNTM4ZTY0MTZmOWY5YWNkOTc2MmRmNDVkM[789]mRmNzI2Yw==");
+}
